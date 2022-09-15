b5a24893b5d838c857a2f2e0c0f9c484341ed980
Tom Booth <tombeezle@gmail.com>
2022-09-15 16:42:34 -0700

    fixed html for climate image, added comments to JS to visualize flow better

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 /* Imports */
 
 /* Get DOM Elements */
+// city name changing with text box
 const cityName = document.getElementById('city-name');
-const cityDisplay = document.getElementById('city-display');
 const cityDisplayName = document.getElementById('city-display-name');
+// architecture changing with selection
+const archSelect = document.getElementById('architecture-select');
+const archDisplay = document.getElementById('architecture-image');
+// climate changing with selection
+const climateSelect = document.getElementById('climate-select');
+const climateDisplay = document.getElementById('climate-image');
+// attraction input box
 
 /* State */
 let city = {
     name: 'Portland',
+    arch: 'metropolis',
+    climate: 'temperate',
 };
 
 /* Events */
+// city name change listener
 cityName.addEventListener('input', () => {
     city.name = cityName.value;
+    displayCity();
+});
+// architecture change listener
+archSelect.addEventListener('change', () => {
+    city.arch = archSelect.value;
+    displayCity();
+});
+// climate change listener
+climateSelect.addEventListener('change', () => {
+    city.climate = climateSelect.value;
     displayCity();
 });
 
 /* Display Functions */
 function displayCity() {
+    // name change function
     cityDisplayName.textContent = city.name;
+    // architecture change function
+    archDisplay.src = 'assets/Arch/' + city.arch + '.jpeg';
+    // climate change function
+    climateDisplay.src = 'assets/climate/' city.climate + 'jpeg';
 }
 
 // (don't forget to call any display functions you want to run on page load!)
