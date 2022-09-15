b98fd05adb2e0ed3dc7b395dcd54a71a8bb96349
Tom Booth <tombeezle@gmail.com>
2022-09-15 15:57:06 -0700

    got my city name to change on text box, super stoked, figured it out finally

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 /* Imports */
 
 /* Get DOM Elements */
+const cityName = document.getElementById('city-name');
+const cityDisplay = document.getElementById('city-display');
+const cityDisplayName = document.getElementById('city-display-name');
 
 /* State */
+let city = {
+    name: 'Portland',
+};
 
 /* Events */
+cityName.addEventListener('input', () => {
+    city.name = cityName.value;
+    displayCity();
+});
 
 /* Display Functions */
+function displayCity() {
+    cityDisplayName.textContent = city.name;
+}
 
 // (don't forget to call any display functions you want to run on page load!)
